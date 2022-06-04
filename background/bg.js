6c68d165488f09eaf437036f9895699802eaffcd
le0nchw <105686548+le0nchw@users.noreply.github.com>
2022-06-05 01:29:23 +0800

    initial commit

---
+++ background/bg.js
@@ -0,0 +1,22 @@
+var db = {
+    warning: ['https://*.xyz/*', 'http://*.xyz/*'],
+    danger: ['https://*/*', 'http://*/']
+}
+
+var sus = ['.xyz/'];
+
+var searchEngine = ['google.com'];
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if('initiator' in details === true){
+            if(details.initiator.includes(searchEngine)){}
+            else if(details.url.includes(sus) && (details.url.includes(details.initiator) === false)) {
+                console.log(details)
+                return { redirectUrl: details.initiator }
+            }
+        }
+    },
+    { urls: ['https://*/*', 'http://*/'] },
+    ['blocking']
+)
