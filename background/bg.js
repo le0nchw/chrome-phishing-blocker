be28caef9d4fbf93243c819aa50da2875a4de745
le0nchw <105686548+le0nchw@users.noreply.github.com>
2022-06-05 03:11:44 +0800

    minor update

--- background/bg.js
+++ background/bg.js
@@ -2,16 +2,26 @@ var sus = ['.xyz/'];
 
 var searchEngine = ['google.com'];
 
+var currentTab = {};
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if('initiator' in details === true){
             if(details.initiator.includes(searchEngine)){}
             else if(details.url.includes(sus) && (details.url.includes(details.initiator) === false)) {
-                console.log(details)
-                return { redirectUrl: details.initiator }
+                getCurrentTab(details.tabId);
+                return { redirectUrl: currentTab.url }
+                
             }
         }
     },
     { urls: ['https://*/*', 'http://*/'] },
     ['blocking']
 )
+
+function getCurrentTab(tabId) {
+    chrome.tabs.get(tabId,  (tab) => {
+        currentTab = tab;
+    });
+  }
+  
