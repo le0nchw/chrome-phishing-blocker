d44aa79c946604c7264ffe0a9d9d90856043f3c7
le0nchw <105686548+le0nchw@users.noreply.github.com>
2022-06-05 15:39:29 +0800

    add multi search engine support

--- background/bg.js
+++ background/bg.js
@@ -1,17 +1,26 @@
 var sus = ['.xyz/'];
 
-var searchEngine = ['google.com'];
+var searchEngine = ['google.com', 'bing.com', 'duckduckgo.com'];
 
 var currentTab = {};
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if('initiator' in details === true){
-            if(details.initiator.includes(searchEngine)){}
-            else if(details.url.includes(sus) && (details.url.includes(details.initiator) === false)) {
-                getCurrentTab(details.tabId);
+            getCurrentTab(details.tabId);
+            const isSearchEngine = searchEngine.find(element => {
+                if (details.initiator.includes(element)) {
+                  return true;
+                }
+            });
+            const isSusUrl = sus.find(element => {
+                if (details.url.includes(element)) {
+                  return true;
+                }
+            });
+            if(isSearchEngine){}
+            else if(isSusUrl && (details.url.includes(details.initiator) === false)) {
                 return { redirectUrl: currentTab.url }
-                
             }
         }
     },
@@ -23,5 +32,5 @@ function getCurrentTab(tabId) {
     chrome.tabs.get(tabId,  (tab) => {
         currentTab = tab;
     });
-  }
+}
   
